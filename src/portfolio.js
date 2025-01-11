14d3146180562b231d373621d1fd23a100e17ccb
ghazafm <fauzan.ghaza@gmail.com>
2025-01-11 07:29:09 +0700

    update resume

--- src/portfolio.js
+++ src/portfolio.js
@@ -23,7 +23,7 @@ const greeting = {
   username: "Ghaza",
   title: "Hi, I'm Fauzan Ghaza Madani",
   subTitle: emoji("Aspiring Data Scientist, Data Engineer, and Network Engineer 🚀"),
-  resumeLink: "https://drive.google.com/file/d/1res13zhGG8Hos0qwbV6vO1Xa4ypOoUkX/view?usp=sharing",
+  resumeLink: "https://drive.google.com/file/d/1_YHFYjetcpbQNQnxCXIP4d3d5mkD7k7N/view?usp=sharing",
   displayGreeting: true
 };
 
